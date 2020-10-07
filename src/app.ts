fa5b1b09f5727ad12122ee1fb59a7d6c4ba63e85
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-10-08 00:03:12 +0400

    about tsconfig

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,36 @@
 
 //# Fix problem 2
 let b1 = document.querySelector('.btn');
+console.log('b1', b1);
 b1?.addEventListener('click', () => {//optional
 	console.log('Button has been clicked!');
 });
+
+
+
+
+
+
+/**++++++++++++++ noImplicitReturns:true ++++++++++++++*/
+//noImplicitReturns:false - no give error when nothing return in function
+// function multiple(a:number, b:number): number {//❌error must return somthing
+//     if(!a && !b) {
+//         return a * b;
+//     }
+// }
+// multiple(2,5)
+
+
+function multiple(a: number, b: number) {//✅
+	//error must return somthing
+	if (!a && !b) {
+		return a * b;
+    }
+    return;
+}
+multiple(2, 5);
+
+
+
+
+/**++++++++++++++ Experimental option - DECORATORS ++++++++++++++*/
